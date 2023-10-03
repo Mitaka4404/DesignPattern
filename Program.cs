75172e8d63e29c4f2738531cefb21aa34d43e890
kimju <kimju@DESKTOP-TU0S1Q9>
2023-10-03 23:34:13 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Practice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.Instance.Method();
+            Singleton.Instance.Method();
+            Singleton.Instance.Method();
+        }
+    }
+
+    public sealed class Singleton
+    {
+        // private constructorを呼び出し
+        // 一つのインスタンスを作成
+        public static readonly Singleton Instance = new Singleton();
+
+        // 外部のクラスが使えないように
+        // priavteに設定
+
+        private Singleton() { } 
+
+        public void Method() 
+        {
+            Console.WriteLine("...");
+        }
+    }
+}
