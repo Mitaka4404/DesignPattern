d4d793d38bbfff7338749feae7df7c159744e4dd
Junhwan Kim <Mitaka4404@gmail.com>
2023-10-13 04:23:27 +0900

    Practice3

---
+++ SingletonPattern/Practice3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+namespace Practice3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Name", typeof(string));
+            DataRow row1 = dt.Rows.Add("Lee");
+            DataRow row2 = dt.Rows.Add("LEE");
+
+            // Singleton インスタンス
+            bool same = DataRowComparer<DataRow>.Default.Equals(row1, row2);
+            Console.WriteLine(same);
+        }
+    }
+}
